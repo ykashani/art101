9b0f83e80c3fb3cba04b98f18a215a1d81ba8a4d
ykashani <ykashani@ucsc.edu>
2022-04-20 13:44:09 -0700

    Lab5

---
+++ lab4/js/lab.js
@@ -0,0 +1,14 @@
+// Summary. (use period)
+// Description. (use period)
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author AuthorName.
+// @since  x.x.x
+
+// Print the board before each turn
+    // Loop through each row
+        // Loop through each space
+            // Print X, O, or Space
+            // Print a | between
+        // Print a newline at the end
+        // Print a horizontal line between
---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+// Summary: (use period)
+// Description.
+// @Link URL
+// @file This files defines the MyClass class.
+// @author Yasmin Kashani
+// @since 2022.4.18
+
+//Define Variables
+make = "Ford";
+model = "Taurus";
+color = "Red";
+year = 2000;
+
+//Calculate
+age = 2022 - year;
+
+//Output
+document. writeIn("Make:" + make + "<br>");
+document. writeIn("Model:" + model + "<br>");
+document. writeIn("Color:" + color + "<br>");
+document. writeIn("Year:" + year + "<br>");
+document. writeIn("Age:" + age + "<br>");
